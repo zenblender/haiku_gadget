f8c30974b172fe6b48ff5818c0f2ab80ba088d1b
Scott Balay <sbalay@gmail.com>
2014-10-06 15:42:29 -0600

    Test script now generates 100 haikus

--- test_haiku.rb
+++ test_haiku.rb
@@ -2,12 +2,17 @@
 
 require File.expand_path('lib/haiku_gadget.rb', File.dirname(__FILE__))
 
-  puts HaikuGadget.haiku "\n"
+  100.times do
 
-  puts "\n"
+    puts HaikuGadget.haiku "\n"
+    puts "\n"
 
-  puts HaikuGadget.top_line
-  puts HaikuGadget.middle_line
-  puts HaikuGadget.bottom_line
+  end
+
+  # puts "\n"
+
+  # puts HaikuGadget.top_line
+  # puts HaikuGadget.middle_line
+  # puts HaikuGadget.bottom_line
 
 
